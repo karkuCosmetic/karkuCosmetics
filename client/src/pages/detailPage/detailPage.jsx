b1fd819d1b384c7c6c4638ede77aaec1019a8eec
ignacio-nicoletti <nicolettiignacio5@gmail.com>
2023-08-25 14:47:24 -0300

    modificar carpeta de lugar

--- client/src/pages/detailPage/detailPage.jsx
+++ client/src/pages/detailPage/detailPage.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
-import {getProductDetail} from '../../utils/fetching';
+import {getProductDetail} from '../../functions/fetching';
 
 export const DetailPage = () => {
   const [detailProduct, SetDetailProduct] = useState ({});
