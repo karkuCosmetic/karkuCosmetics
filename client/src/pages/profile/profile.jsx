c77ac36d32f98fb9a59e4258605e16e881bd3af0
ignacio-nicoletti <nicolettiignacio5@gmail.com>
2023-08-30 17:24:11 -0300

    updates

--- client/src/pages/profile/profile.jsx
+++ client/src/pages/profile/profile.jsx
@@ -4,11 +4,11 @@ import {DecodedToken} from '../../utils/DecodedToken';
 import {getUserDetail} from '../../functions/fetchingUsers';
 import {fileUpload} from '../../utils/fileUpload';
 import {updateProduct} from '../../functions/fetchingProducts';
+import ProductUpload from '../../components/UserAdmin/ProductUpload/productUpload';
 
 export const Profile = () => {
   const [profile, SetProfile] = useState ({});
-  const [images, setImages] = useState ([]); // buffer de images
-  const [imageurls, setImageurls] = useState (null); //array de urls de cloudinary
+
 
   useEffect (() => {
     const token = GetDecodedCookie ('cookieToken');
@@ -23,34 +23,14 @@ export const Profile = () => {
     SetProfile (info);
   };
 
-  const handleImageUpload = event => {
-    const selectedFiles = event.target.files;
-    setImages (prevImages => [...prevImages, ...selectedFiles]);
-  };
-
-  const handlerSubmitImage = async () => {
-    await fileUpload (images, 'profiles').then (res => {
-      setImageurls (res);
-      updateProduct (res);
-      console.log (res);
-    });
-  };
+ 
 
   //Maapear profile 
 
-  //las funciones hay que corregirlas por que el componente esta creado en prueba para products/admin
+
   return (
     <div>
-      <input
-        type="file"
-        name=""
-        id=""
-        onChange={e => handleImageUpload (e)} //input y button se usarian en el admin 
-        multiple
-      />
-      <button onClick={handlerSubmitImage} disabled={images ? false : true}>
-        Subir foto
-      </button>
+   
 
     </div>
   );
