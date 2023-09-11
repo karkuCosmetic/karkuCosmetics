96936d3b03248733dde003776470f5efba01663e
ignacio-nicoletti <nicolettiignacio5@gmail.com>
2023-09-11 17:40:50 -0300

    password new

--- client/src/components/ForgotPassword/forgotPassword.jsx
+++ client/src/components/ForgotPassword/forgotPassword.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {useState} from 'react';
-
 import './forgotPassword.css';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faArrowLeft} from '@fortawesome/free-solid-svg-icons';
