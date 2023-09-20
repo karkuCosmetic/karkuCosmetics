0514ecda13dcd070b7a05fe7d5a36f243f2b908e
ignacio-nicoletti <nicolettiignacio5@gmail.com>
2023-09-20 17:39:37 -0300

    delete

--- client/src/utils/DecodedCookie.js
+++
@@ -1,6 +0,0 @@
-export function GetDecodedCookie(name) {
-    let matches = document.cookie.match(new RegExp(
-      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-    ));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
-  }
