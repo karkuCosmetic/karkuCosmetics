5c1e838cc31586147112e7fb78afdc041b74abdf
ignacio-nicoletti <nicolettiignacio5@gmail.com>
2023-09-29 17:57:43 -0300

    updates

--- client/src/functions/Auth.js
+++ client/src/functions/Auth.js
@@ -2,16 +2,12 @@ import axios from "axios";
 
 export const postlogin = async (values) => {
   try {
-    const resp = await axios.post(
-      "https://karku-cosmetics-4dsy.vercel.app/login",
-      values
-    );
-
+    const resp = await axios.post("http://localhost:3001/login", values);
     let token = resp.data.token;
     document.cookie =
       encodeURIComponent("cookieToken") + "=" + encodeURIComponent(token);
-    // localStorage.setItem("token", token);
-    return { token, status: resp.status, session: true };
+
+    return { token, status: resp.status, verify: resp.data.verify };
   } catch (err) {
     console.log(err);
   }
