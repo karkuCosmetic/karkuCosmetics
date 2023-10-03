6c5fe8625ae85f99be6fb8d73ccbe6a623319f91
ignacio-nicoletti <nicolettiignacio5@gmail.com>
2023-10-03 12:26:57 -0300

    fux bug

--- Server/src/middlewares/validationUser.js
+++ Server/src/middlewares/validationUser.js
@@ -1,34 +1,23 @@
-import { validationResult, body } from "express-validator"
-
+import { validationResult, body } from "express-validator";
 
 export const validationError = (req, res, next) => {
-    const errors = validationResult(req)
+  const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
-    }
-    next()
-}
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
 
 export const validationUseOrpassRegister = [
+  body("email", "Formato de email incorrecto").trim().isEmail(),
+  body("password", "minimo 6 caracteres").trim().isLength({ min: 6 }),
 
-    body("email", "Formato de email incorrecto")
-        .trim()
-        .isEmail()
-        .normalizeEmail(),
-    body("password", "minimo 6 caracteres").trim().isLength({ min: 6 }),
-
-    validationError
-
-
-
-]
+  validationError,
+];
 
 export const bodyLoginValidator = [
-    body("email", "Formato de email incorrecto")
-        .trim()
-        .isEmail()
-        .normalizeEmail(),
-    body("password", "Mínimo 6 carácteres").trim().isLength({ min: 6 }),
-    validationError,
+  body("email", "Formato de email incorrecto").trim().isEmail(),
+  body("password", "Mínimo 6 carácteres").trim().isLength({ min: 6 }),
+  validationError,
 ];
