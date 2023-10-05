689e405fc992eee38f791b1e9ac2c9a0f7dcfffe
ignacio-nicoletti <nicolettiignacio5@gmail.com>
2023-10-05 13:40:38 -0300

    updates

--- client/src/functions/emails.js
+++ client/src/functions/emails.js
@@ -8,10 +8,20 @@ export const getEmails = async () => {
     console.log(error);
   }
 };
+
 export const getEmailsById = async (id) => {
   try {
     const emails = await axios.get(`http://localhost:3001/admin/email/${id}`);
     return emails;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteEmailById = async (id) => {
+  try {
+    const res = await axios.delete(`http://localhost:3001/admin/email/${id}`);
+    return res.data;
   } catch (error) {
     console.log(error);
   }
