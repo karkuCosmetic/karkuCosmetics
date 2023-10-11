dbc1a0a5bb8cbe94a4fe3b3531f08da3677642bb
ignacio-nicoletti <nicolettiignacio5@gmail.com>
2023-10-11 10:25:11 -0300

    updates

--- client/src/components/Admin/ProductGroup/PreviewProduct/PreviewProduct.jsx
+++ client/src/components/Admin/ProductGroup/PreviewProduct/PreviewProduct.jsx
@@ -1,66 +1,66 @@
-import React, { useEffect, useState } from "react";
-import Modal from "react-modal";
+import React, {useEffect, useState} from 'react';
+import Modal from 'react-modal';
 import {
   getProduct,
   DeleteProductById,
   getProductDetail,
-} from "../../../../functions/fetchingProducts";
-import "./PreviewProduct.css";
-import EditProduct from "../Products/EditProduct";
+} from '../../../../functions/fetchingProducts';
+import './PreviewProduct.css';
+import EditProduct from '../Products/EditProduct';
 
-const PreviewProduct = ({ setSection }) => {
-  const [products, setProducts] = useState([]);
-  const [showAllProducts, setShowAllProducts] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [deletingProductId, setDeletingProductId] = useState(null);
-  const [selectedProduct, setSelectedProduct] = useState(null);
-  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
-  const [selectedProductDetails, setSelectedProductDetails] = useState(null);
+const PreviewProduct = ({setSection}) => {
+  const [products, setProducts] = useState ([]);
+  const [showAllProducts, setShowAllProducts] = useState (false);
+  const [searchTerm, setSearchTerm] = useState ('');
+  const [deletingProductId, setDeletingProductId] = useState (null);
+  const [selectedProduct, setSelectedProduct] = useState (null);
+  const [isEditModalOpen, setIsEditModalOpen] = useState (false);
+  const [selectedProductDetails, setSelectedProductDetails] = useState (null);
 
-  useEffect(() => {
-    getProduct()
-      .then((data) => setProducts(data))
-      .catch((error) => console.error(error));
-    window.scrollTo(0, 0);
+  useEffect (() => {
+    getProduct ()
+      .then (data => setProducts (data))
+      .catch (error => console.error (error));
+    window.scrollTo (0, 0);
   }, []);
 
-  const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value);
+  const handleSearchChange = event => {
+    setSearchTerm (event.target.value);
   };
 
-  const filteredProducts = products.filter((product) =>
-    product.title.toLowerCase().includes(searchTerm.toLowerCase())
+  const filteredProducts = products.filter (product =>
+    product.title.toLowerCase ().includes (searchTerm.toLowerCase ())
   );
 
-  const handleDeleteProduct =(productId) => {
-    setSelectedProduct(productId);
+  const handleDeleteProduct = productId => {
+    setSelectedProduct (productId);
     // DeleteProductById(productId)
   };
 
-  const handleEditProduct = async (productId) => {
-    setSelectedProduct(productId);
-    setIsEditModalOpen(true);
+  const handleEditProduct = async productId => {
+    setSelectedProduct (productId);
+    setIsEditModalOpen (true);
     try {
-      const productData = await getProductDetail(productId);
-      setSelectedProductDetails(productData);
+      const productData = await getProductDetail (productId);
+      setSelectedProductDetails (productData);
     } catch (error) {
-      console.error("Error al obtener detalles del producto:", error);
+      console.error ('Error al obtener detalles del producto:', error);
     }
   };
 
-  const handleDeleteConfirmation =  () => {
+  const handleDeleteConfirmation = () => {
     if (selectedProduct !== null) {
       try {
-     DeleteProductById(selectedProduct);
+        DeleteProductById (selectedProduct);
 
-        const updatedProducts = products.filter(
-          (product) => product._id !== selectedProduct
+        const updatedProducts = products.filter (
+          product => product._id !== selectedProduct
         );
-        setProducts(updatedProducts);
+        setProducts (updatedProducts);
 
-        setSelectedProduct(null);
+        setSelectedProduct (null);
       } catch (error) {
-        console.error("Error al eliminar producto:", error);
+        console.error ('Error al eliminar producto:', error);
       }
     }
   };
@@ -76,39 +76,35 @@ const PreviewProduct = ({ setSection }) => {
       />
       <ul>
         {showAllProducts
-          ? products.map((product) => (
-              <li key={product._id}>{product.title}</li>
-            ))
-          : filteredProducts.slice(0, 5).map((product) => (
+          ? products.map (product => <li key={product._id}>{product.title}</li>)
+          : filteredProducts.slice (0, 5).map (product => (
               <li key={product._id}>
                 <img src={product.image[0]} alt={product.title} />
                 {product.title} ${product.price}
-                <button onClick={() => handleEditProduct(product._id)}>
+                <button onClick={() => handleEditProduct (product._id)}>
                   Editar
                 </button>
-                <button onClick={() => handleDeleteProduct(product._id)}>
+                <button onClick={() => handleDeleteProduct (product._id)}>
                   Eliminar
                 </button>
               </li>
             ))}
       </ul>
 
-      {!showAllProducts && (
-        <button onClick={() => setSection("Product")}>Ver todos</button>
-      )}
+      {!showAllProducts &&
+        <button onClick={() => setSection ('Product')}>Ver todos</button>}
 
-      {selectedProduct !== null && (
+      {selectedProduct !== null &&
         <div>
           <p>Confirmar Borrado de producto?</p>
           <button onClick={handleDeleteConfirmation}>Confirmar</button>
-        </div>
-      )}
+        </div>}
       <Modal
         isOpen={isEditModalOpen}
-        onRequestClose={() => setIsEditModalOpen(false)}
+        onRequestClose={() => setIsEditModalOpen (false)}
       >
         <EditProduct
-          match={{ params: { id: selectedProduct } }}
+          match={{params: {id: selectedProduct}}}
           productDetails={selectedProductDetails}
         />
       </Modal>
