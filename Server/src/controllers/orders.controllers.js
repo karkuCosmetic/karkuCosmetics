931a1c611615b9b629c8a8f5f1ab37f6f2124a31
ignacio-nicoletti <nicolettiignacio5@gmail.com>
2023-10-13 11:58:51 -0300

    updates

--- Server/src/controllers/orders.controllers.js
+++ Server/src/controllers/orders.controllers.js
@@ -13,7 +13,6 @@ export const getOrders = async (req, res) => {
 export const getOrderById = async (req, res) => {
   try {
     const { id } = req.params;
-
     const admin = await Admin.findOne({});
     let order = admin.orders.find((el) => el.id === id);
     res.status(200).json({ orders: order });
@@ -30,13 +29,13 @@ export const updateOrders = async (req, res) => {
     // Actualiza el campo "entrega" en Admin
     await Admin.updateMany(
       { "orders.id": id },
-      { $set: { "orders.$.entrega": status } }
+      { $set: { "orders.$.detailPay.status": status } }
     );
 
     // Actualiza el campo "entrega" en User
     await User.updateMany(
       { "buys.id": id },
-      { $set: { "buys.$.entrega": status } }
+      { $set: { "buys.$.detailPay.status": status } }
     );
 
     res.status(200).json("elementos actualizados");
