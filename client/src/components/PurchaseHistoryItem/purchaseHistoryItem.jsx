3db9ff17b44efc7cd43a930c7a55d6e5f4124653
ignacio-nicoletti <nicolettiignacio5@gmail.com>
2023-10-16 16:52:26 -0300

    profile fix

--- client/src/components/PurchaseHistoryItem/purchaseHistoryItem.jsx
+++ client/src/components/PurchaseHistoryItem/purchaseHistoryItem.jsx
@@ -1,34 +1,36 @@
-import React from "react";
-import "./purchaseHistoryItem.css";
-const PurchaseHistoryItem = ({ purchase }) => {
-  const formatDateModal = (isoDate) => {
-    const date = new Date(isoDate);
-    const day = String(date.getDate()).padStart(2, "0");
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const year = date.getFullYear();
+import React from 'react';
+import './purchaseHistoryItem.css';
+const PurchaseHistoryItem = ({purchase}) => {
+  const formatDateModal = isoDate => {
+    const date = new Date (isoDate);
+    const day = String (date.getDate ()).padStart (2, '0');
+    const month = String (date.getMonth () + 1).padStart (2, '0');
+    const year = date.getFullYear ();
     return `${day}/${month}/${year}`;
   };
 
   const calculateTotal = () => {
-    if (!purchase.TotalPagado) {
+    if (!purchase.methodPay.total) {
       let total = 0;
-      for (const item of purchase.itemsComprados) {
+      for (const item of purchase.detailPay.items) {
         total += item.unit_price * item.quantity;
       }
-      return total > 0 ? `$${total}` : "";
+      return total > 0 ? `$${total}` : '';
     }
-    return "";
+    return '';
   };
 
   return (
     <div className="purchase-item">
       <div className="purchase-total">
-        <h4 className="date-purchase">{formatDateModal(purchase.fecha)} -</h4>
-        {purchase.TotalPagado && `Total: $${purchase.TotalPagado}`}
-        {!purchase.TotalPagado && `Total: ${calculateTotal()} `}
-        <div>- {purchase.entrega}</div>
+        <h4 className="date-purchase">
+          {formatDateModal (purchase.methodPay.datePay)} -
+        </h4>
+        {purchase.methodPay.total && `Total: $${purchase.methodPay.total}`}
+        {!purchase.methodPay.total && `Total: ${calculateTotal ()} `}
+        <div>- {purchase.detailPay.status}</div>
       </div>
-      {purchase.itemsComprados.map((item, index) => (
+      {purchase.detailPay.items.map ((item, index) => (
         <div className="purchase-item-details" key={index}>
           <img src={item.picture_url} alt={item.title} className="item-image" />
           <div className="item-title">{item.title}</div>
