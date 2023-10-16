031800e7b6714613c59c17fe8ac6202ee012fe60
Jona Garcia <jalexisagarcia.91@gmail.com>
2023-10-16 13:13:01 -0300

    messages

--- client/src/components/Admin/MessageGroup/PreviewMessage/PreviewMessage.jsx
+++ client/src/components/Admin/MessageGroup/PreviewMessage/PreviewMessage.jsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { getEmails } from "../../../../functions/emails";
 import "./PreviewMessage.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const PreviewMessage = ({ setSection }) => {
   const [notifications, setNotifications] = useState([]);
-  console.log(notifications);
+  const [selectedMessage, setSelectedMessage] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [replyText, setReplyText] = useState("");
+
   useEffect(() => {
     getEmails()
       .then((data) => setNotifications(data.emails))
@@ -12,6 +17,36 @@ const PreviewMessage = ({ setSection }) => {
   }, []);
 
   const slicedNotifications = notifications.slice(0, 5);
+
+  const openModal = (message) => {
+    setSelectedMessage(message);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setSelectedMessage(null);
+    setIsModalOpen(false);
+  };
+
+  // const handleReply = () => {
+  //   if (selectedMessage && replyText.trim() !== "") {
+  //     const replyBody = `Mensaje original:\n${selectedMessage.user_message}\n\nRespuesta:\n${replyText}`;
+
+  //     SendEmail({
+  //       to: selectedMessage.user_email,
+  //       subject: "Re: Respuesta a tu mensaje",
+  //       body: replyBody,
+  //     })
+  //       .then(() => {
+  //         console.log("Mensaje enviado con éxito");
+
+  //         setReplyText("");
+  //       })
+  //       .catch((error) => {
+  //         console.error("Error al enviar el mensaje:", error);
+  //       });
+  //   }
+  // };
 
   return (
     <div className="preview-message">
@@ -29,9 +64,19 @@ const PreviewMessage = ({ setSection }) => {
                     <br />
                     <strong>Teléfono:</strong> {el.user_phone}
                     <br />
+                    <strong>Fecha:</strong> {el.user_phone}
+                    <br />
                   </div>
                   <div className="text-message-preview-container">
                     <strong>Mensaje:</strong> {el.user_message}
+                  </div>
+                  <div className="btn-show-message-container">
+                  <button
+                    className="btn-show-message"
+                    onClick={() => openModal(el)}
+                  >
+                    Ver mensaje
+                  </button>
                   </div>
                 </div>
               </li>
@@ -41,7 +86,58 @@ const PreviewMessage = ({ setSection }) => {
           )}
         </ul>
       </div>
-      <button onClick={() => setSection("Message")}>Ver Todos</button>
+      <div className="btn-showAll-messages-container">
+        <button
+          className="btn-showAll-messages"
+          onClick={() => setSection("Message")}
+        >
+          Ver Todos
+        </button>
+      </div>
+      {isModalOpen && (
+        <div className="overlay">
+          <div className="message-modal">
+            <div className="close-message-modal-container">
+              <span className="close-message-modal" onClick={closeModal}>
+                <FontAwesomeIcon icon={faTimes} />
+              </span>
+            </div>
+            {selectedMessage && (
+              <div className="message-details">
+                <div className="info-message-modal">
+                  <div className="name-email">
+                    <strong>Nombre:</strong> {selectedMessage.user_name}
+                    <br />
+                    <strong>Correo:</strong> {selectedMessage.user_email}
+                  </div>
+                  <div className="phone-date">
+                    <strong>Teléfono:</strong> {selectedMessage.user_phone}
+                    <br />
+                    <strong>Fecha:</strong> {selectedMessage.date}
+                  </div>
+                </div>
+                <div className="text-message-modal">
+                  <strong>Mensaje:</strong> {selectedMessage.user_message}
+                </div>
+                <textarea
+                  className="textarea-response-message"
+                  placeholder="Escribir respuesta acá..."
+                  value={replyText}
+                  onChange={(e) => setReplyText(e.target.value)}
+                />
+                <div className="btn-response-message-container">
+                  <button
+                    className="btn-response-message"
+                    //  onClick={handleReply}
+                  >
+                    Responder
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
