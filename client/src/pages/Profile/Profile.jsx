b3cb0a7ea3a8c1b8758bbb24393c6c277dc9c9b3
Jona Garcia <jalexisagarcia.91@gmail.com>
2023-11-13 20:26:08 -0300

    styles

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -386,7 +386,9 @@ const Profile = () => {
                           {selectedPurchase.detailPay.status}
                         </p>
                         <p>
-                          <strong>Envío: </strong>
+                          <strong>
+                            Envío: {selectedPurchase.detailPay.shipPrice}{" "}
+                          </strong>
                           {selectedPurchase.detailPay.shipStatus === false ? (
                             <>
                               Pendiente de pago
