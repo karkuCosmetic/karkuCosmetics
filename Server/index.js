e8e890178f6260f767285fb53b53414d650add91
ignacio-nicoletti <nicolettiignacio5@gmail.com>
2023-11-13 11:33:18 -0300

    updates

--- Server/index.js
+++ Server/index.js
@@ -18,7 +18,7 @@ const whiteList = [
   "http://localhost:3000",
   "https://www.mercadopago.com.ar",
   "https://www.mercadolibre.com",
-  "karku-cosmetics-4dsy-git-main-karkucosmetic.vercel.app",
+  "https://karku-cosmetics-4dsy-karkucosmetic.vercel.app",
 
   // Agrega aquí otros orígenes permitidos si es necesario
 ];
