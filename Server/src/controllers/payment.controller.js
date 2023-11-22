52d156af4344e79f4720d6c0b80e9a02c4420cb2
ignacio-nicoletti <nicolettiignacio5@gmail.com>
2023-11-21 22:49:18 -0300

    up´dates

--- Server/src/controllers/payment.controller.js
+++ Server/src/controllers/payment.controller.js
@@ -130,7 +130,7 @@ export const reciveWebhook = async (req, res) => {
         payer: {
           name: user.name,
           lastName: user.lastName,
-          DNI: data.response.card.cardholder.identification.number,
+          DNI: data.response.card.cardholder.identification.number || {},
           phonePerson: user.phone,
           emailPayer: data.response.payer.email,
           email: data.response.metadata.email,
